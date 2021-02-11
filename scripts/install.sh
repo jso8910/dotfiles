cd643f8b4b7ffb0d9fda92b886774b3b9af97f41
Jason Rosenzweig <mail4jasonr@gmail.com>
2021-02-11 20:28:45 +0800

    please

--- scripts/install.sh
+++ scripts/install.sh
@@ -65,9 +65,11 @@ print 'Installing nerd-fonts-complete'
 install_pkg wget
 mkdir -p build
 cd build
-git clone https://aur.archlinux.org/nerd-fonts-complete.git
+#git clone https://aur.archlinux.org/nerd-fonts-complete.git
+yay --getpkgbuild nerd-fonts-complete
 cd nerd-fonts-complete
 wget -O nerd-fonts-2.1.0.tar.gz https://github.com/ryanoasis/nerd-fonts/archive/v2.1.0.tar.gz
+
 makepkg -sci BUILDDIR=.
 
 
